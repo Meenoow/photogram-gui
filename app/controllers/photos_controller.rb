193a30ccfc4cc7c4a29d6162a248656140c99c1f
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-04-20 21:26:24 +0000

    Photo page is working Able to add and delete images!

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -35,16 +35,17 @@ class PhotosController < ApplicationController
   def create
     # Parameters: {"query_image"=>"v", "query_caption"=>"v", "query_owner_id_box"=>"11"}
     input_image = params.fetch("query_image")
-    input_image = params.fetch("query_caption") 
-    input_image = params.fetch("query_owner_id")
+    input_caption = params.fetch("query_caption") 
+    input_owner_id = params.fetch("query_owner_id")
 
     a_new_photo = Photo.new
     a_new_photo.image = input_image
     a_new_photo.caption = input_caption
     a_new_photo.owner_id = input_owner_id
 
-    a_new_photo.save
+    a_new_photo.save 
 
-    render(({ :template => "photo_templates/create.html.erb" }))
+    #render(({ :template => "photo_templates/create.html.erb" }))
+    redirect_to("/photos/" + a_new_photo.id.to_s)
   end  
 end
